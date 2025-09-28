91d311cc0215ffb6d8582c3a7ee3c9801e245fd2
HabitualCoder <jaydeep.geekcoder@gmail.com>
2025-09-28 18:12:14 +0530

    added packages

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+
+
+
 def main():
     print("Hello from langchain-course!")
 
